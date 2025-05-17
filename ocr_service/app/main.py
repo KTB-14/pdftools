3d63715877e9d5fb36f3ef5f017084ed9b194389
KTB-14 <127084301+KTB-14@users.noreply.github.com>
2025-05-17 20:37:06 +0200

    Added Front End

--- ocr_service/app/main.py
+++ ocr_service/app/main.py
@@ -17,6 +17,15 @@ app.include_router(upload.router, prefix="", tags=["upload"])
 app.include_router(status.router, prefix="", tags=["status"])
 app.include_router(download.router, prefix="", tags=["download"])
 
+# Autoriser CORS côté FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou ["http://192.168.1.127"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
     return {"message": "Bienvenue sur l'OCR Service"}
