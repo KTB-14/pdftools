21f84e6464bc7865e9f431aa2e10951f9e344411
Brook <brook-tamrat.kifle@univ-rouen.fr>
2025-05-21 20:10:09 +0200

    Dev Final with front

--- frontend/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// Redirige à la page choisie ou affiche un message
-document.querySelectorAll('.card').forEach(card => {
-  card.addEventListener('click', e => {
-    if (card.classList.contains('disabled')) {
-      alert('Service à venir bientôt !');
-      e.preventDefault();
-    }
-  });
-});
