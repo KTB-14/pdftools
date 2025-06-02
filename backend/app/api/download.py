300ada95f1927c05ed367babbde8740e32346acf
KIFLE <bkifle.Valdepharm@fareva.com>
2025-06-02 13:49:01 +0200

    Revert "Version Avant Modif Front fixtest 1.1 update"
    This reverts commit 80b2549446d75b05a69da2f10b197a178155c785.

--- backend/app/api/download.py
+++ backend/app/api/download.py
@@ -38,33 +38,19 @@ def download_single_or_multiple(job_id: str):
         logger.exception(f"[{job_id}] ❌ Erreur pendant la tentative de téléchargement : {e}")
         raise HTTPException(status_code=500, detail=f"Erreur : {e}")
 
-<<<<<<< HEAD
+
 @router.get("/download/{job_id}/{original_name}")
 def download_specific_file(job_id: str, original_name: str):
     status_path = config.OCR_ROOT / job_id / config.STATUS_FILENAME
     if not status_path.exists():
         raise HTTPException(status_code=404, detail="Status non trouvé")
-=======
 
-@router.get("/download/{job_id}/{filename}")
-def download_specific_file(job_id: str, filename: str):
-    """
-    Permet de télécharger un fichier PDF individuel traité
-    """
-    file_path = config.OCR_ROOT / job_id / config.OUTPUT_SUBDIR / filename
-    logger.info(f"[{job_id}] 📨 Requête de téléchargement fichier individuel : {filename}")
->>>>>>> parent of 80b2549 (Version Avant Modif Front fixtest 1.1 update)
-
-    if not file_path.exists() or not file_path.is_file():
-        logger.warning(f"[{job_id}] ❌ Fichier {filename} introuvable")
-        raise HTTPException(status_code=404, detail="Fichier non trouvé")
-
-<<<<<<< HEAD
-        from urllib.parse import unquote
-        original_name_decoded = unquote(original_name)
+    try:
+        with open(status_path, "r", encoding="utf-8") as f:
+            status_data = json.load(f)
 
         file_entry = next(
-            (f for f in status_data.get("files", []) if f["original"] == original_name_decoded),
+            (f for f in status_data.get("files", []) if f["original"] == original_name),
             None
         )
 
@@ -78,18 +64,10 @@ def download_specific_file(job_id: str, filename: str):
 
         return FileResponse(
             path=str(file_path),
-            filename=file_entry["final_name"],  # 🟢 Envoie final_name ici
+            filename=original_name,
             media_type="application/pdf"
         )
 
     except Exception as e:
         logger.exception(f"[{job_id}] ❌ Erreur pendant le téléchargement : {e}")
         raise HTTPException(status_code=500, detail=f"Erreur : {str(e)}")
-=======
-    return FileResponse(
-        path=str(file_path),
-        filename=filename,
-        media_type="application/pdf"
-    )
- 
->>>>>>> parent of 80b2549 (Version Avant Modif Front fixtest 1.1 update)
