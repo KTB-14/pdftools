db48a2043940104df25b705d0d798a888d7d980d
KTB-14 <127084301+KTB-14@users.noreply.github.com>
2025-06-04 01:07:32 +0200

    FrontEnd / Backend Functional  testing dashboard

--- frontend/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-// Gestion des cartes désactivées
-document.querySelectorAll('.card').forEach(card => {
-  card.addEventListener('click', e => {
-    if (card.classList.contains('disabled')) {
-      e.preventDefault();
-      alert('Ce service sera bientôt disponible !');
-    }
-  });
-}); 
