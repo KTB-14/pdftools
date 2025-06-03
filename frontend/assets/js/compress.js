71666af5fb5e19d8421a059be08048f309d3cad8
KIFLE <bkifle.Valdepharm@fareva.com>
2025-06-03 11:52:17 +0200

    Front Visuel Amelioration1.1

--- frontend/assets/js/compress.js
+++ frontend/assets/js/compress.js
@@ -1,4 +1,4 @@
-const API_BASE = "api";
+const API_BASE = "/api";
 const dropzone = document.getElementById('dropzone');
 const fileInput = document.getElementById('fileInput');
 const selectBtn = document.getElementById('selectFile');
